de6b861023ab793e4a2b29c2dfa6a03585c86b62
Dell arca <arcacorini@gmail.com>
2025-06-28 14:56:22 -0400

    Agregado soporte para renderizado de html

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as nunjucks from 'nunjucks';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  nunjucks.configure(join(__dirname, '..', 'views'), {
+    autoescape: true,
+    express: app,
+  });
+
+  app.setViewEngine('html');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 
 @Controller('reports')
@@ -6,7 +6,12 @@ export class ReportsController {
   constructor(private readonly databaseService: DatabaseService) {}
 
   @Get()
-  prueba() {
-    return this.databaseService.select('select * from usuario;');
+  @Render('index')
+  async prueba() {
+    const data = await this.databaseService.select('select * from usuario;');
+
+    return {
+      data,
+    };
   }
 }
